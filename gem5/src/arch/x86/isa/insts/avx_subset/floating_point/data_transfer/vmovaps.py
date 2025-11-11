0a366e70676fabf0c17a05851547a3bcd855eee4
Karthikeya Kotha <kkotha2@wisc.edu>
2025-11-10 18:37:44 -0600

    changes after vmovups

--- gem5/src/arch/x86/isa/insts/avx_subset/floating_point/data_transfer/vmovaps.py
+++ gem5/src/arch/x86/isa/insts/avx_subset/floating_point/data_transfer/vmovaps.py
@@ -1,23 +1,12 @@
 microcode = '''
-def macroop VMOVAPS_XMM_XMM {
-    movfp xmm0, xmm0m, dataSize=8
-    movfp xmm1, xmm1m, dataSize=8
-    vclear dest=xmm2, destVL=16
-};
 def macroop VMOVAPS_XMM_M {
     ldfp xmm0, seg, sib, "DISPLACEMENT", dataSize=8
     ldfp xmm1, seg, sib, "DISPLACEMENT + 8", dataSize=8
     vclear dest=xmm2, destVL=16
 };
-def macroop VMOVAPS_M_XMM {
+def macroop VMOVAPS_M_XMM_M {
     stfp xmm0, seg, sib, "DISPLACEMENT", dataSize=8
     stfp xmm1, seg, sib, "DISPLACEMENT + 8", dataSize=8
-};
-def macroop VMOVAPS_YMM_YMM {
-    movfp xmm0, xmm0m, dataSize=8
-    movfp xmm1, xmm1m, dataSize=8
-    movfp xmm2, xmm2m, dataSize=8
-    movfp xmm3, xmm3m, dataSize=8
 };
 def macroop VMOVAPS_YMM_M {
     ldfp xmm0, seg, sib, "DISPLACEMENT", dataSize=8
@@ -25,7 +14,7 @@ def macroop VMOVAPS_YMM_M {
     ldfp xmm2, seg, sib, "DISPLACEMENT + 16", dataSize=8
     ldfp xmm3, seg, sib, "DISPLACEMENT + 24", dataSize=8
 };
-def macroop VMOVAPS_M_YMM {
+def macroop VMOVAPS_M_YMM_M {
     stfp xmm0, seg, sib, "DISPLACEMENT", dataSize=8
     stfp xmm1, seg, sib, "DISPLACEMENT + 8", dataSize=8
     stfp xmm2, seg, sib, "DISPLACEMENT + 16", dataSize=8
