b928731864687fa0805e7792f48a037bbef8dc81
Karthikeya Kotha <kkotha2@wisc.edu>
2025-11-10 18:50:42 -0600

    added _p vmov instructions

--- gem5/src/arch/x86/isa/insts/avx_subset/floating_point/data_transfer/vmovups.py
+++ gem5/src/arch/x86/isa/insts/avx_subset/floating_point/data_transfer/vmovups.py
@@ -4,9 +4,20 @@ def macroop VMOVUPS_XMM_M {
     ldfp xmm1, seg, sib, "DISPLACEMENT + 8", dataSize=8
     vclear dest=xmm2, destVL=16
 };
+def macroop VMOVUPS_XMM_P {
+    rdip t7
+    ldfp xmm0, seg, riprel, "DISPLACEMENT", dataSize=8
+    ldfp xmm1, seg, riprel, "DISPLACEMENT + 8", dataSize=8
+    vclear dest=xmm2, destVL=16
+};
 def macroop VMOVUPS_M_XMM_M {
     stfp xmm0, seg, sib, "DISPLACEMENT", dataSize=8
     stfp xmm1, seg, sib, "DISPLACEMENT + 8", dataSize=8
+};
+def macroop VMOVUPS_M_XMM_P {
+    rdip t7
+    stfp xmm0, seg, riprel, "DISPLACEMENT", dataSize=8
+    stfp xmm1, seg, riprel, "DISPLACEMENT + 8", dataSize=8
 };
 def macroop VMOVUPS_YMM_M {
     ldfp xmm0, seg, sib, "DISPLACEMENT", dataSize=8
@@ -14,10 +25,24 @@ def macroop VMOVUPS_YMM_M {
     ldfp xmm2, seg, sib, "DISPLACEMENT + 16", dataSize=8
     ldfp xmm3, seg, sib, "DISPLACEMENT + 24", dataSize=8
 };
+def macroop VMOVUPS_YMM_P {
+    rdip t7
+    ldfp xmm0, seg, riprel, "DISPLACEMENT", dataSize=8
+    ldfp xmm1, seg, riprel, "DISPLACEMENT + 8", dataSize=8
+    ldfp xmm2, seg, riprel, "DISPLACEMENT + 16", dataSize=8
+    ldfp xmm3, seg, riprel, "DISPLACEMENT + 24", dataSize=8
+};
 def macroop VMOVUPS_M_YMM_M {
     stfp xmm0, seg, sib, "DISPLACEMENT", dataSize=8
     stfp xmm1, seg, sib, "DISPLACEMENT + 8", dataSize=8
     stfp xmm2, seg, sib, "DISPLACEMENT + 16", dataSize=8
     stfp xmm3, seg, sib, "DISPLACEMENT + 24", dataSize=8
+};
+def macroop VMOVUPS_M_YMM_P {
+    rdip t7
+    stfp xmm0, seg, riprel, "DISPLACEMENT", dataSize=8
+    stfp xmm1, seg, riprel, "DISPLACEMENT + 8", dataSize=8
+    stfp xmm2, seg, riprel, "DISPLACEMENT + 16", dataSize=8
+    stfp xmm3, seg, riprel, "DISPLACEMENT + 24", dataSize=8
 };
 '''
