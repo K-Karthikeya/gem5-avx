be9b7f9fa6258470ab40549b35df3fc43174cc4a
Karthikeya Kotha <kkotha2@wisc.edu>
2025-11-11 12:40:49 -0600

    Testing avx2 with assembly

--- gem5/src/arch/x86/isa/insts/avx_subset/floating_point/arithmetic/vmulps.py
+++ gem5/src/arch/x86/isa/insts/avx_subset/floating_point/arithmetic/vmulps.py
@@ -25,6 +25,18 @@ def macroop VMULPS_XMM_XMM_P {
     vmulf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
     vclear dest=xmm2, destVL=16
 };
+def macroop VMULPS_XMM_XMM_XMM_M {
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
+    vmulf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
+    vclear dest=xmm2, destVL=16
+};
+def macroop VMULPS_XMM_XMM_XMM_P {
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
+    vmulf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
+    vclear dest=xmm2, destVL=16
+};
 def macroop VMULPS_YMM_YMM {
     vmulf dest=xmm0, src1=xmm0v, src2=xmm0m, size=4, VL=32
 };
@@ -46,6 +58,20 @@ def macroop VMULPS_YMM_YMM_M {
     vmulf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
 };
 def macroop VMULPS_YMM_YMM_P {
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
+    ldfp ufp2, seg, sib, "DISPLACEMENT + 16", dataSize=8
+    ldfp ufp3, seg, sib, "DISPLACEMENT + 24", dataSize=8
+    vmulf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
+};
+def macroop VMULPS_YMM_YMM_XMM_M {
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
+    ldfp ufp2, seg, sib, "DISPLACEMENT + 16", dataSize=8
+    ldfp ufp3, seg, sib, "DISPLACEMENT + 24", dataSize=8
+    vmulf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
+};
+def macroop VMULPS_YMM_YMM_XMM_P {
     ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
     ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
     ldfp ufp2, seg, sib, "DISPLACEMENT + 16", dataSize=8
--- gem5/src/arch/x86/isa/insts/avx_subset/floating_point/arithmetic/vxorps.py
+++ gem5/src/arch/x86/isa/insts/avx_subset/floating_point/arithmetic/vxorps.py
@@ -25,6 +25,18 @@ def macroop VXORPS_XMM_XMM_P {
     vxorf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
     vclear dest=xmm2, destVL=16
 };
+def macroop VXORPS_XMM_XMM_XMM_M {
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
+    vxorf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
+    vclear dest=xmm2, destVL=16
+};
+def macroop VXORPS_XMM_XMM_XMM_P {
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
+    vxorf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
+    vclear dest=xmm2, destVL=16
+};
 def macroop VXORPS_YMM_YMM {
     vxorf dest=xmm0, src1=xmm0v, src2=xmm0m, size=4, VL=32
 };
@@ -46,6 +58,20 @@ def macroop VXORPS_YMM_YMM_M {
     vxorf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
 };
 def macroop VXORPS_YMM_YMM_P {
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
+    ldfp ufp2, seg, sib, "DISPLACEMENT + 16", dataSize=8
+    ldfp ufp3, seg, sib, "DISPLACEMENT + 24", dataSize=8
+    vxorf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
+};
+def macroop VXORPS_YMM_YMM_XMM_M {
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
+    ldfp ufp2, seg, sib, "DISPLACEMENT + 16", dataSize=8
+    ldfp ufp3, seg, sib, "DISPLACEMENT + 24", dataSize=8
+    vxorf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
+};
+def macroop VXORPS_YMM_YMM_XMM_P {
     ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
     ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
     ldfp ufp2, seg, sib, "DISPLACEMENT + 16", dataSize=8
