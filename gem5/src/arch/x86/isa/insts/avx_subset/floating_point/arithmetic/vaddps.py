c9b0a1c047876848d84410102160dc39abc22c58
Karthikeya Kotha <kkotha2@wisc.edu>
2025-11-11 12:37:38 -0600

    Testing avx2 with assembly

--- gem5/src/arch/x86/isa/insts/avx_subset/floating_point/arithmetic/vaddps.py
+++ gem5/src/arch/x86/isa/insts/avx_subset/floating_point/arithmetic/vaddps.py
@@ -15,6 +15,18 @@ def macroop VADDPS_XMM_M {
     vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
     vclear dest=xmm2, destVL=16
 };
+def macroop VADDPS_XMM_XMM_M {
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
+    vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
+    vclear dest=xmm2, destVL=16
+};
+def macroop VADDPS_XMM_XMM_P {
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
+    vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=16
+    vclear dest=xmm2, destVL=16
+};
 def macroop VADDPS_YMM_YMM {
     vaddf dest=xmm0, src1=xmm0v, src2=xmm0m, size=4, VL=32
 };
@@ -23,6 +35,20 @@ def macroop VADDPS_YMM_YMM_XMM_XMM {
     vaddf dest=xmm0, src1=xmm0v, src2=xmm0m, size=4, VL=32
 };
 def macroop VADDPS_YMM_M {
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
+    ldfp ufp2, seg, sib, "DISPLACEMENT + 16", dataSize=8
+    ldfp ufp3, seg, sib, "DISPLACEMENT + 24", dataSize=8
+    vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
+};
+def macroop VADDPS_YMM_YMM_M {
+    ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
+    ldfp ufp2, seg, sib, "DISPLACEMENT + 16", dataSize=8
+    ldfp ufp3, seg, sib, "DISPLACEMENT + 24", dataSize=8
+    vaddf dest=xmm0, src1=xmm0v, src2=ufp0, size=4, VL=32
+};
+def macroop VADDPS_YMM_YMM_P {
     ldfp ufp0, seg, sib, "DISPLACEMENT", dataSize=8
     ldfp ufp1, seg, sib, "DISPLACEMENT + 8", dataSize=8
     ldfp ufp2, seg, sib, "DISPLACEMENT + 16", dataSize=8
